f7382af7c2109fd54b136aeb3ecb72bf204f5be5
Pako-12317 <patrykm10@nycstudents.net>
2025-02-24 14:27:02 +0000

    ,commit

--- src/main/java/com/example/project/Selection_Sort/SelectionSort.java
+++ src/main/java/com/example/project/Selection_Sort/SelectionSort.java
@@ -7,50 +7,51 @@ import java.io.File;
 public class SelectionSort {
 
     // PART A. implementing selection sort
-    public static int[] selectionSort(int[] elements) {
-        for (int j = 0; j < elements.length - 1; j++) {
-            int minIndex = j;
-            for (int k = j + 1; k < elements.length; k++) {
-                if (elements[k] < elements[minIndex]) {
-                    minIndex = k;
+    public static int[] selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallestIdx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[smallestIdx]) {
+                    smallestIdx = j;
                 }
             }
-            int temp = elements[j];
-            elements[j] = elements[minIndex];
-            elements[minIndex] = temp;
+            int swap = arr[i];
+            arr[i] = arr[smallestIdx];
+            arr[smallestIdx] = swap;
         }
 
-        return elements;
+        return arr;
     }
 
 
     // PART B. sorting a 1000-word list
-    public static ArrayList<String> selectionSortWordList(ArrayList<String> words) {
-        for (int j = 0; j < words.size() - 1; j++) {
-            int minIndex = j;
-            for (int k = j + 1; k < words.size(); k++) {
-                if (words.get(k).compareTo(words.get(minIndex)) < 0) {
-                    minIndex = k;
+    public static ArrayList<String> selectionSortWordList(ArrayList<String> wordArray) {
+        for (int i = 0; i < wordArray.size() - 1; i++) {
+            int smallestIdx = i;
+            for (int j = i + 1; j < wordArray.size(); j++) {
+                if (wordArray.get(j).compareTo(wordArray.get(smallestIdx)) < 0) {
+                    smallestIdx = j;
                 }
             }
-            String temp = words.get(j);
-            words.set(j, words.get(minIndex));
-            words.set(minIndex, temp);
+            String swap = wordArray.get(i);
+            wordArray.set(i, wordArray.get(smallestIdx));
+            wordArray.set(smallestIdx, swap);
         }
-        return words;
+        return wordArray;
     }
 
     //call this method to load 1000 words into list. Use it to test Part B
-    public static ArrayList<String> loadWordsInto(ArrayList<String> wordList) {
+    public static ArrayList<String> loadWordsInto(ArrayList<String> wordArray) {
         try {
-            Scanner input = new Scanner(new File("src/main/java/com/example/project/Selection_Sort/words.txt"));
-            while (input.hasNext()) {
-                String word = input.next();
-                wordList.add(word);
+            Scanner fileScanner = new Scanner(new File("src/main/java/com/example/project/Selection_Sort/words.txt"));
+            while (fileScanner.hasNext()) {
+                String term = fileScanner.next();
+                wordArray.add(term);
+                fileScanner.close();
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        return wordList;
+        return wordArray;
     }
 }
